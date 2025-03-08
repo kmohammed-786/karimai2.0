9f27bb3319988ebd2c015e839c6446421c6bd0d7
kmohammed-786 <79663828+kmohammed-786@users.noreply.github.com>
2025-03-07 22:54:57 -0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -403,7 +403,7 @@ const contactInfo = {
 // Twitter Section
 
 const twitterDetails = {
-  userName: "twitter", //Replace "twitter" with your twitter username without @
+  userName: "servicenowdev9", //Replace "twitter" with your twitter username without @
   display: true // Set true to display this section, defaults to false
 };
 
