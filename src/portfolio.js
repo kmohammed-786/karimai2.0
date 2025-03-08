10de29ff1b502c21845ddc19368b23354ff2e35f
kmohammed-786 <79663828+kmohammed-786@users.noreply.github.com>
2025-03-07 22:37:02 -0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -10,7 +10,7 @@ import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your
 const splashScreen = {
   enabled: true, // set false to disable splash screen
   animation: splashAnimation,
-  duration: 10 // Set animation duration as per your animation
+  duration: 50 // Set animation duration as per your animation
 };
 
 // Summary And Greeting Section
